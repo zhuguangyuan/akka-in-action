70e83e75dc3b08711c2cf06b420a83c207aeab27
zhuguangyuan <zhuguangyuan27@163.com>
2019-06-25 11:32:06 +0800

    tdd 有一个测试用例出错，找不到TestActorRef?原因待查

--- chapter-testdriven/src/test/scala/aia/testdriven/SilentActor01Test.scala
+++ chapter-testdriven/src/test/scala/aia/testdriven/SilentActor01Test.scala
@@ -11,34 +11,74 @@ class SilentActor01Test extends TestKit(ActorSystem("testsystem"))
   with MustMatchers
   with StopSystemAfterAll {
   // Commented to make the travis build pass, this is the original test in the book
+  "A Silent Actor" must {
+    "change state when it receives a message, single threaded" in {
+      //Write the test, first fail
+      fail("not implemented yet")
+      // import SilentActor._
+
+      // val silentActor = TestActorRef[SilentActor] // 找不到 TestActorRef???
+      // silentActor ! SilentMessage("whisper")
+      // silentActor.underlyingActor.state must (contain("whisper"))
+    }
+    "change state when it receives a message, multi-threaded" in {
+      //Write the test, first fail
+      // fail("not implemented yet")
+      import SilentActor._
+
+      val silentActor = system.actorOf(Props[SilentActor],"s3")
+      silentActor ! SilentMessage("whisper1")
+      silentActor ! SilentMessage("whisper2")
+      silentActor ! GetState(testActor)
+
+      expectMsg(Vector("whisper1", "whisper2","__"))
+    }
+  }
   // "A Silent Actor" must {
-  //   "change state when it receives a message, single threaded" in {
+  //   "change state when it receives a message, single threaded" ignore {
   //     //Write the test, first fail
   //     fail("not implemented yet")
   //   }
-  //   "change state when it receives a message, multi-threaded" in {
+  //   "change state when it receives a message, multi-threaded" ignore {
   //     //Write the test, first fail
   //     fail("not implemented yet")
   //   }
   // }
-  "A Silent Actor" must {
-    "change state when it receives a message, single threaded" ignore {
-      //Write the test, first fail
-      fail("not implemented yet")
-    }
-    "change state when it receives a message, multi-threaded" ignore {
-      //Write the test, first fail
-      fail("not implemented yet")
-    }
-  }
 
 }
 
 
+
+// class SilentActor extends Actor {
+//   def receive = {
+//     case msg =>
+//   }
+// }
+
+object SilentActor {
+  case class SilentMessage(data : String)
+  case class GetState(receiver : ActorRef)
+}
 
 class SilentActor extends Actor {
+  import SilentActor._
+  var internalState = Vector[String]()
+
   def receive = {
-    case msg =>
+    case SilentMessage(data) => 
+        internalState = internalState :+ data
+    case GetState(receiver) =>
+        receiver ! internalState :+ "__"
   }
+
+  def state = internalState
 }
+
+
+
+
+
+
+
+
 
