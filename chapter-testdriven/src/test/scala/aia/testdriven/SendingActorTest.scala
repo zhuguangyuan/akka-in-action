0dc9934a9e97bd4886b7aa23d82e3c34df06ddd3
zhuguangyuan <zhuguangyuan27@163.com>
2019-06-25 14:30:23 +0800

    测试发送消息的Actor 通过

--- chapter-testdriven/src/test/scala/aia/testdriven/SendingActorTest.scala
+++ chapter-testdriven/src/test/scala/aia/testdriven/SendingActorTest.scala
@@ -5,6 +5,9 @@ import akka.testkit.TestKit
 import akka.actor.{ Props, ActorRef, Actor, ActorSystem }
 import org.scalatest.{WordSpecLike, MustMatchers}
 
+/**
+ * 将一个 Vector[Event] 发送给一个 SendingActor 它将进行排序然后发回给 testActor
+ */
 class SendingActorTest extends TestKit(ActorSystem("testsystem"))
   with WordSpecLike
   with MustMatchers
@@ -24,7 +27,7 @@ class SendingActorTest extends TestKit(ActorSystem("testsystem"))
       }.toVector
 
       val unsorted = randomEvents()
-      val sortEvents = SortEvents(unsorted)
+      val sortEvents = SortEvents
       sendingActor ! sortEvents
 
       expectMsgPF() {
@@ -40,8 +43,8 @@ object SendingActor {
   def props(receiver: ActorRef) =
     Props(new SendingActor(receiver))
   case class Event(id: Long)  
-  case class SortEvents(unsorted: Vector[Event])  
-  case class SortedEvents(sorted: Vector[Event])
+  case class SortEvents(unsorted: Vector[Event])  // 接收到别的 actor 发送来的 unsorted 后的处理函数
+  case class SortedEvents(sorted: Vector[Event])  // 将 unsorted 排序完成后的处理函数
 }
 
 class SendingActor(receiver: ActorRef) extends Actor {
